e424bbb1544502cfe2432d3900750947bd527c0d
nzapisocki <nzapisoc@ualberta.ca>
2026-05-11 22:17:21 -0600

    Update main.py
    Added token cleaning and removing stopwords, added functionality for adding documents to the matrix

--- search-engine/main.py
+++ search-engine/main.py
@@ -1,6 +1,11 @@
 # this file can be for building the inverted index
 
 import nltk
+from nltk.corpus import stopwords
+from collections import Counter
+from collections import defaultdict
+
+nltk.download('stopwords')
 nltk.download('punkt_tab')
 
 ''' read the text file in as a string 
@@ -22,10 +27,42 @@ def tokenize_files(text):
     return nltk.tokenize.word_tokenize(text, "english", False)
 
 
+''' Clean documents '''
+
+
+def clean_documents(tokens):
+    # get english stop words
+    stop_words = set(stopwords.words('english'))
+
+    # clean documents
+    tokens = [t.lower() for t in tokens]
+    tokens = [t for t in tokens if t.isalpha()]
+    tokens = [t for t in tokens if t not in stop_words]
+
+    return tokens
+
+
+''' add book to term document matrix '''
+
+
+def add_book(cleaned_tokens, doc_id):
+    # go through words, calculate tf, add the word to the dictionary, store id and tf in the postings
+    index = defaultdict(dict)
+
+    counts = Counter(cleaned_tokens)
+
+    for term, tf in counts.items():
+        index[term][doc_id] = tf
+
+    return index
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    doc = read_files("documents/pg40868.txt")
-    print(doc)
-    print(tokenize_files(doc))
+    doc = read_files("documents/hamlet.txt")
+    tokens = clean_documents(tokenize_files(doc))
+    print(tokens)
+
+    print(add_book(tokens, 0))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
